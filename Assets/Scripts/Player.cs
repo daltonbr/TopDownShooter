3d7b40215feaa79a7bce8d9cc078341c6ab681c2
Dalton Lima <sacinopatinete@gmail.com>
2017-03-31 20:30:34 -0300

    Add Player Scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate()
+	{
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+
+    public void Move(Vector3 _velocity)
+    {
+        this.velocity = _velocity;
+    }
+}

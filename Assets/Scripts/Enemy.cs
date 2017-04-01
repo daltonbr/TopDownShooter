97fb2f2a7a5980a4f8434156a27edb572aaac6f1
daltonbr <sacinopatinete@gmail.com>
2017-03-31 22:15:14 -0300

    Add Enemy prefab with NavMeshAgent

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+
+	NavMeshAgent pathfinder;
+	Transform target;
+
+	void Start () {
+		pathfinder = GetComponent<NavMeshAgent>();
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		StartCoroutine(UpdatePath ());
+	}
+
+	IEnumerator UpdatePath()
+	{
+		float refreshRate = 0.25f;
+
+		while (target != null)
+		{
+			Vector3 targePosition = new Vector3(target.position.x, 0, target.position.z);
+			pathfinder.SetDestination (target.position);
+			yield return new WaitForSeconds(refreshRate);
+		}
+	}
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+	public LayerMask collisionMask;
 	float speed = 10f;
 
 	public void SetSpeed(float newSpeed)
@@ -13,6 +14,25 @@ public class Projectile : MonoBehaviour {
 		
 	void Update ()
 	{
+		float moveDistance = speed * Time.fixedDeltaTime;
+		CheckCollisions (moveDistance);
 		this.transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+
+	void CheckCollisions(float moveDistance)
+	{
+		Ray ray = new Ray(this.transform.position, this.transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+		{
+			OnHitObject(hit);
+		}
+	}
+
+	void OnHitObject(RaycastHit hit)
+	{
+		print(hit.collider.gameObject.name);
+		GameObject.Destroy(this.gameObject);
 	}
 }

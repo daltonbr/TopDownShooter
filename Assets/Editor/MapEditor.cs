b6ba69bca8c5a23f84821a0d4b028c1050ca7680
Dalton Lima <sacinopatinete@gmail.com>
2017-04-03 00:51:16 -0300

    minor improvement on the mapGeneration
    just updating the mapGeneration when we change values in the inspector

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -8,8 +8,24 @@ public class MapEditor : Editor {
     {
         base.OnInspectorGUI();
 
-        MapGenerator map = target as MapGenerator;
+        // Always updating ... cpu intensive sometimes
+        //MapGenerator map = target as MapGenerator;
+        //map.GenerateMap();
 
-        map.GenerateMap();
+        // just update when some value is changed in the inspector (much more effective)
+        if (GUI.changed)
+        {
+            //Debug.Log("GUI changed");
+            MapGenerator map = target as MapGenerator;
+            map.GenerateMap();
+        }
+
+        // or update just when we press this buttom
+        if (GUILayout.Button("Generate Map"))
+        {
+            Debug.Log("Generating map");
+            //MapGenerator map = target as MapGenerator;
+            //map.GenerateMap();
+        }
     }
 }

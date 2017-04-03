3c665efbc48d700fc62af298f3eafa222e04480c
Dalton Lima <sacinopatinete@gmail.com>
2017-04-02 22:37:02 -0300

    Add Obstacle Placement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,14 +7,21 @@ public class MapGenerator : MonoBehaviour
 {
 
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range(0,1)]
     public float outlinePercent;
 
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
+
+    public int seed = 10;
+
     private void Awake()
     {
         Assert.IsNotNull(tilePrefab);
+        Assert.IsNotNull(obstaclePrefab);
         Assert.AreNotEqual(0f, mapSize.x, "MapSize.x couldn't be 0");
         Assert.AreNotEqual(0f, mapSize.y, "MapSize.y couldn't be 0");
     }
@@ -25,6 +32,16 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+
         string holderName = "GeneratedMap";
         if (transform.FindChild(holderName))
         {
@@ -39,7 +56,7 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0f, -mapSize.y/2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x, y);
                 
                 //Instantiate our tile rotate 90 degrees, in order to be flat on the ground
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
@@ -47,5 +64,39 @@ public class MapGenerator : MonoBehaviour
                 newTile.parent = mapHolder;
             }
         }
+
+        int obstacleCount = 10;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity) as Transform;
+            newObstacle.parent = mapHolder;
+        }
     }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0f, -mapSize.y / 2 + 0.5f + y);
+    }
+        
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
 }

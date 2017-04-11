a960b3aa71687437f7944006d92d3fcb93d52f78
daltonbr <sacinopatinete@gmail.com>
2017-04-11 10:12:30 -0300

    Add Point ScriptableObject

---
+++ Assets/Scripts/ProjectRelated/Point.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : ScriptableObject
+{
+	public string pointName;
+	public Coord coord;
+	public Color thisColor = Color.white;
+	public PointConnection[] connections;
+}
+
+[System.Serializable]
+public class Coord
+{
+	public int x;
+	public int y;
+}
+
+[System.Serializable]
+public class PointConnection
+{
+	public string name;
+	public float distance;
+}
+

b053e7329777379c85b369670d8fb86349f458f9
Dalton Lima <daltonvarussa@gmail.com>
2017-06-20 20:20:37 -0300

    Polishing HealthPack

--- Assets/Scripts/HealthPack.cs
+++ Assets/Scripts/HealthPack.cs
@@ -8,19 +8,22 @@ public class HealthPack : MonoBehaviour {
 	//public GameObject healthPack;
 	public float rotateSpeed = 50f;	// Degree per second
 	public float recoverPercentage = 1f;
+    public ParticleSystem hpEffect;
 
 	public event System.Action OnCollected;
 
-	void Awake () {
-		
-	}
+    public void Awake()
+    {
+        Assert.IsNotNull(hpEffect, "[HealthPack] hpEffect can't be null!");
+    }
 
-	void FixedUpdate () {
+
+    void FixedUpdate () {
 		this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
 	}
 
-	void OnTriggerEnter(Collider other) {
-		//Debug.Log("Healthpack entered by " + other.name);
+	void OnTriggerStay(Collider other) {
+		//Debug.Log("Healthpack in contact with " + other.name);
 		if (other.tag == "Player")
 		{
 //			Debug.Log("Healthpack entered by " + other.name);
@@ -30,6 +33,8 @@ public class HealthPack : MonoBehaviour {
 				player.RefillHealth();
 				if (OnCollected != null)
 				{
+                    AudioManager.instance.PlaySound("PickupHealth", this.transform.position);
+                    Destroy(Instantiate(hpEffect.gameObject, this.transform.position, Quaternion.Euler(new Vector3(90, 0, 0))), hpEffect.main.duration);
 					OnCollected();
 				}
 				Destroy(this.gameObject);
